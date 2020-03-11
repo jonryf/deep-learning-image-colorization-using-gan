f5266b2cc3f9caccd04199aedd61d34c6b3a44b1
Alex Polus <alex.polus13@gmail.com>
2020-03-11 15:36:39 -0700

    fixed ssim

--- utils.py
+++ utils.py
@@ -1,9 +1,17 @@
 from dataloader import ImageDataset, get_loader
 from settings import BATCH_SIZE, SHUFFLE
-
+import pytorch_ssim
+import torch
+from torch.autograd import Variable
 
 def get_datasets():
     train_dataset = get_loader(ImageDataset("trainIds.csv"), BATCH_SIZE, SHUFFLE)
     test_dataset = get_loader(ImageDataset("testIds.csv"), BATCH_SIZE, SHUFFLE)
     return train_dataset, test_dataset
 
+def similarity(img1, img2):
+
+    if torch.cuda.is_available():
+        img1 = img1.cuda()
+        img2 = img2.cuda()
+    return pytorch_ssim.ssim(img1, img2)
