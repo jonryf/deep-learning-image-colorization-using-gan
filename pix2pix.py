325c7a9c08a9cdba7b35fbe72fac7063fa212508
root <root@DESKTOP-OR6I109.localdomain>
2020-03-11 14:59:17 -0700

    modified training loop

--- pix2pix.py
+++ pix2pix.py
@@ -8,6 +8,48 @@ from torch.nn import CrossEntropyLoss
 from torch.optim import Adam
 
 
+def trainPix2Pix(model, data, batchSize=1 ,totalEpochs=50, genLr=0.0001, descLr=0.00005):
+    genOptimizer = Adam( list(model.gen.parameters()), lr=genLr)
+    discOptimizer = Adam( list(model.disc.parameters()), lr=descLr)
+    criterion = CrossEntropyLoss()
+    model.gen.train()
+    model.disc.train()
+    for epoch in totalEpochs:
+        for index in range(0, data.size()[0], batchSize):
+            curData = data[index:index + batchSize,:,:,:]
+            gradientStep(model, genInputs, discImputs, genOptimizer, discOptimizer)
+
+
+# assumes minibatch is only colord images.
+def gradientStepPix2Pix(model, minibatch, criterion, genOptimizer, discOptimizer):
+    origBW = blackandWhite(minibatch)
+    origColor = minibatch
+
+    # train descriminator
+    genColor = model.generate(origBW)
+    genPairs = torch.cat((origBW, genColor), 3)
+    origPairs = torch.cat((origBW, origColor), 3)
+
+    descTrainSet = torch.cat((genPairs, origPairs), 0)
+    genLabels = torch.zeros(genPairs.size()[0])
+    origLabels =  torch.ones(origPairs.size()[0])
+    labels = torch.cat(genLabels,origLabels),0))
+    preds = model.discriminate(descTrainSet)
+    batch_loss = criterion(preds, labels)
+
+    model.gen.zero_grad()
+    model.disc.zero_grad()
+    batch_loss.backward()
+    discOptimizer.step()
+
+    preds = model.discriminate(genPairs)
+    batch_loss = criterion(preds,  torch.ones(genLabels.size()[0]))
+    model.gen.zero_grad()
+    model.disc.zero_grad()
+    batch_loss.backward()
+    genOptimizer.step()
+
+
 class pix2pix():
 
     def __init__(self):
@@ -16,12 +58,6 @@ class pix2pix():
         self.gen = UNET(numclasses, numchannels)
         self.disc = Discriminator()
         self.criterion = CrossEntropyLoss()
-        self.transform = transforms.Compose([
-            transforms.functional.to_grayscale(img, num_output_channels=1)
-        ])
-        self.trainData = []
-        self.
-
 
     def generate(self, greyscale):
         return self.gen.forward(greyscale)
@@ -30,40 +66,4 @@ class pix2pix():
     def discriminate(self, img):
         #(images, features, height, width)
         # Return average - 1 value for all images
-
         patch_values = self.disc.forward(img)
-
-        pass
-
-    def train(self, all_batches, totalEpochs=50, genLr=0.0001, descLr=0.00005):
-        genOptimizer = Adam( list(self.gen.parameters()), lr=genLr)
-        discOptimizer = Adam( list(self.disc.parameters()), lr=descLr)
-        for epoch in totalEpochs:
-            for minibatch in all_batches:
-                #(images, features, height, width) assume black and white images are paired in the features chanel
-                origBW = blackandWhite(minibatch)
-                origColor = minibatch
-
-                # train descriminator
-                genColor = self.generate(origBW)
-                genPairs = torch.cat((origBW, genColor), 3)
-                origPairs = torch.cat((origBW, origColor), 3)
-
-                descTrainSet = torch.cat((genPairs, origPairs), 0)
-                genLabels = torch.zeros(genPairs.size()[0])
-                origLabels =  torch.ones(origPairs.size()[0])
-                labels = torch.cat(genLabels,origLabels),0))
-                preds = self.discriminate(descTrainSet)
-                batch_loss = self.criterion(preds, labels)
-
-                self.gen.zero_grad()
-                self.disc.zero_grad()
-                batch_loss.backward()
-                discOptimizer.step()
-
-                preds = self.discriminate(genPairs)
-                batch_loss = self.criterion(preds,  torch.ones(genLabels.size()[0]))
-                self.gen.zero_grad()
-                self.disc.zero_grad()
-                batch_loss.backward()
-                genOptimizer.step()
